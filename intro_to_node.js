aaa833f7685b478acbfb1a25ef0beed03bbf6eb7
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-10-28 10:10:18 -0600

    Update

--- intro_to_node.js
+++ intro_to_node.js
@@ -19,13 +19,13 @@ if (!fs.existsSync(target)) {
 linesInSource.forEach(line => {
   // get the content of the lines, first word is a filename, rest is content
   const [ filename, ...contentArr ] = line.split(' ');
+
   // construct the full path for the file to create
   const newFilePath = path.join(__dirname, target, filename);
 
   // write the file and it's contents
   fs.writeFileSync(
-    newFilePath,
-    contentArr,
+    newFilePath, contentArr.join(' '),
     { flag: 'w+', encoding: 'utf-8' }
   );
 });
